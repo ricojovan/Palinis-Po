d13e92d78aae556c1eadb21b3788bf4ece264929
ricojovan <ricopitpit07@gmail.com>
2024-04-17 00:08:07 +0800

    aa

--- HRMSystem/register.php
+++ HRMSystem/register.php
@@ -148,7 +148,7 @@
     </div>
   </div>
   <div class="headings *:text-center *:leading-4">
-    <p class="text-xl font-serif font-semibold text-[#434955]">FIRST NAME LAST NAME</p>
+    <div class="f-name"><p class="text-xl font-serif font-semibold text-[#434955]">FIRST NAME LAST NAME</p></div>
     <p class="text-sm font-semibold text-[#434955]">POSITION</p>
   </div>
   <div class="w-full items-center justify-center flex">
@@ -165,7 +165,7 @@
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
 </svg>
-        <p>Username</p>
+        <div class="username"><p>Username</p></div>
       </li>
       <li>
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
