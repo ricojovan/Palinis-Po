450a25fff6b038d751480115ea408045c98efa46
ricojovan <ricopitpit07@gmail.com>
2024-04-29 13:37:56 +0800

    added etms

--- HRMSystem/assets/JS/main.js
+++ HRMSystem/assets/JS/main.js
@@ -1,9 +1,11 @@
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
-const container = document.getElementById('container');
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
 
-signUpButton.addEventListener('click', () =>
-container.classList.add('right-panel-active'));
+sign_up_btn.addEventListener("click", () => {
+  container.classList.add("sign-up-mode");
+});
 
-signInButton.addEventListener('click', () =>
-container.classList.remove('right-panel-active'));
+sign_in_btn.addEventListener("click", () => {
+  container.classList.remove("sign-up-mode");
+});
--- HRMSystem/login_and_sign-in_form.php
+++ HRMSystem/login_and_sign-in_form.php
@@ -1,55 +1,47 @@
-
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login HRM - Signin/Signup</title>
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
-        integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
-    <link rel="stylesheet" href="assets/CSS/login-signin_styling.css">
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-
-</head>
-
-<body>
-<div class="flex justify-center"> 
-<div class="login-card-1">
-  <div class="card-header-1">
-    <h1>Login</h1>
-  </div>
-  <div class="card-body-1">
-    <form action="login.php" method="post">
-      <div class="form-group-1">
-        <label for="username">Username</label>
-        <input type="text" name="login_username" required="">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <script
+      src="https://kit.fontawesome.com/64d58efce2.js"
+      crossorigin="anonymous"
+    ></script>
+    <link rel="stylesheet" href="assets/CSS/login-signin_styling.css" />
+    <title>Sign in & Sign up Form</title>
+  </head>
+  <body>
+    <div class="container">
+      <div class="forms-container">
+        <div class="signin-signup">
+          <form action="login.php" method="post" class="sign-in-form">
+            <h2 class="title">Sign in</h2>
+            <div class="input-field">
+              <i class="fas fa-user"></i>
+              <input type="text" name="login_username" required="" placeholder="Username" />
+            </div>
+            <div class="input-field">
+              <i class="fas fa-lock"></i>
+              <input type="password" name="login_password" required="" placeholder="Password" />
+            </div>
+            <button type="submit" class="btn solid login-button" >Login</button>
+          </form>
+        </div>
       </div>
-      <div class="form-group-1">
-        <label for="password">Password</label>
-        <input type="password" name="login_password" required="">
-      </div>
-      <div class="form-group-1">
-        <button type="submit" class="login-button">Login</button>
-      </div>
-    </form>
-  </div>
-</div>
-</div>
-        <!-- TRY LANGGGGGGGGGGGGGGGGGGGGG -->
 
-        <!-- <div class="container">
+      <div class="panels-container">
+        <div class="panel left-panel">
+          <div class="content">
+            <h3>Welcome to Palinis po!</h3>
+            <p>
+              Enter your details and start your journey with us!
+            </p>
+          </div>
+          <img src="assets\images\media\logo.png" class="image" alt="" />
+        </div>
+      </div>
+    </div>
 
-        <div class="form-container sign-in-container">
-            <form action="login.php" method="post">
-                <h1>Sign in</h1>
-                <input type="text" name="login_username" placeholder="Username" required />
-                <input type="password" name="login_password" placeholder="Password" required />
-                <button type="submit">Sign In</button>
-            </form>
-        </div> -->
-    
     <script src="assets/JS/main.js"></script>
 </body>
 
